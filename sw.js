594c4ff50385b2ac411fbf12709861d8fa7953d2
Joseph Oun <joun@ebay.com>
2022-04-06 10:04:41 +0300

    change sw implementation

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
-self.addEventListener('push', () => {
-    self.registration.sendNotification('testmessage', {})
-})
+self.addEventListener('push', function(e) {
+   var options = {
+       body: 'Test Message',
+       vibrate: [100,50,100],
+       data: {
+           dateOfArrival: Date.now(),
+           primaryKey: '2'
+       },
+       actions: [
+           {
+               action: 'explore', title: 'Explore'
+           },
+           {
+               action: 'close', title: 'Close'
+           }
+       ]
+   }
+
+   e.waitUntil(
+       self.registration.showNotification('Test Message', options)
+   );
+});
